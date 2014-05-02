67aed6d22e33ac8da51fbce9c91a228af303e33a
Robson Sousa <robsonsky@gmail.com>
2014-05-01 22:26:27 -0300

    01-05

--- src/com/adapp/adapter/ProductListAdapter.java
+++ src/com/adapp/adapter/ProductListAdapter.java
@@ -3,29 +3,29 @@ package com.adapp.adapter;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.adapp.CategoryListActivity.ListViewItem;
 import com.adapp.ProductListActivity;
+import com.adapp.ProductListActivity.ListViewItem;
 import com.adapp.R;
 import com.adapp.R.layout;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.webkit.WebView.FindListener;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class CategoryListAdapter extends BaseAdapter {
+public class ProductListAdapter extends BaseAdapter {
 	
-	public List<ListViewItem> items;
+	public List<com.adapp.CategoryListActivity.ListViewItem> items;
 	private Context context;
 	private ArrayList<ListViewItem> arrayList;
 	
-	public CategoryListAdapter(Activity context, List<ListViewItem> items) {
+	public ProductListAdapter(Activity context, List<ListViewItem> items) {
 		this.context = context;
 		this.items = items;
 	}
@@ -37,59 +37,52 @@ public class CategoryListAdapter extends BaseAdapter {
 		this.arrayList.addAll(items);
 		return this.arrayList;
 	}
-
+	
 	@Override
 	public int getCount() {
 		return items.size();
 	}
-
+	
 	@Override
 	public Object getItem(int position) {
 		return items.get(position);
 	}
-
+	
 	@Override
 	public long getItemId(int position) {
 		return position;
 	}
-
+	
 	@Override
 	public View getView(final int position, View convertView, ViewGroup parent) {
-		
 		final ListViewItem item = items.get(position);
 		final ViewHolder holder;
 		if (convertView == null) {
 			LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			convertView = li.inflate(R.layout.category_row, parent, false);
+			convertView = li.inflate(R.layout.product_row, parent, false);
 			holder = new ViewHolder();
 			holder.textViewName = (TextView) convertView.findViewById(R.id.product_row_name);
-			holder.textViewTotal = (TextView) convertView.findViewById(R.id.product_row_price);
+			holder.textViewPrice = (TextView) convertView.findViewById(R.id.product_row_price);
+			holder.textViewLocation = (TextView) convertView.findViewById(R.id.product_row_location);
+			holder.imageView = (ImageView) convertView.findViewById(R.id.product_row_image);
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
-		holder.textViewName.setText(item.name);
-		holder.textViewTotal.setText(item.total);
 		
-		convertView.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(context, ProductListActivity.class);
-				intent.putExtra("idlabel", item.id);
-				
-				context.startActivity(intent);
-			}
-		});
-		return convertView;
+		holder.textViewName.setText(item.productName);
+		holder.textViewPrice.setText(item.productPrice);
+		holder.textViewLocation.setText(item.productLocation);
+		
 	}
 	
 	public static class ViewHolder {
-		TextView textViewName, textViewTotal;
+		TextView textViewName, textViewPrice, textViewLocation;
+		ImageView imageView;
 	}
 	
 	@Override
-	public int getViewTypeCount(){
+	public int getViewTypeCount() {
 		return getCount();
 	}
 	
@@ -97,5 +90,4 @@ public class CategoryListAdapter extends BaseAdapter {
 	public int getItemViewType(int position) {
 		return position;
 	}
-
 }
